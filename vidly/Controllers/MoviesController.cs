7671e2e2fd3e7f5950b353e546d2b44fe181b7ab
shayanshahid <shayanshahid@vs215>
2018-07-16 05:08:26 +0000

    Add project files.

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            
+              var movie = new Movie() {Name ="Shayan" };
+
+            return View(movie);
+        }
+        [Route("movies/Edit/id")]
+        public ActionResult Edit(int id)
+        {
+            return Content("id is equal to" + id);
+        }
+        public ActionResult Index(int? PageIndex, String Sortby)
+        {
+            if (!PageIndex.HasValue)
+                PageIndex = 1;
+            if (String.IsNullOrWhiteSpace(Sortby))
+                Sortby = "Name";
+            return Content(String.Format("pageindex={0}&sortby={1}", PageIndex, Sortby));
+        }
+        [Route ("movies/ByReleaseDate/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year +"heello"+month);
+        }
+            
+        } 
+      
+    }
